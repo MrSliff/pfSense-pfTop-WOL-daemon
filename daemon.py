e86afaabee82ae171f78bbd04e18b9887f6f80e0
MrSliff <larsmie@unitybox.de>
2022-09-25 19:15:41 +0200

    add mqtt

--- daemon.py
+++ daemon.py
@@ -1,6 +1,7 @@
 import os
-from daemonize import Daemonize
 from time import sleep
+from daemonize import Daemonize
+#import paho.mqtt.client as mqtt
 
 host = "192.168.20.3"
 
@@ -11,8 +12,10 @@ pid = "/tmp/pftop_wake.pid"
 def main ():
     while True:
         
-        #output = os.popen('pftop -b -a -f "dst host "' + host + ' | grep 4:4')
         output = os.popen('pftop -b -a -f "dst host "' + host + ' | grep 2:0').readlines()
+        
+        #if len(output) > 0:
+            
 
         for i in output:
             print (i)
