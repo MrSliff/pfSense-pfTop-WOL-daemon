0abe1d6a202e947d12fc6d0589142e73d47d4516
MrSliff <larsmie@unitybox.de>
2022-09-25 17:29:08 +0200

    first tests

--- daemon.py
+++ daemon.py
@@ -4,4 +4,9 @@ host = "192.168.20.3"
 
 result = os.popen('pftop -b -a -f "dst host "' + host ' | grep 4:4')
 
+
 print (result.read())
+
+print("Established connections: " + len(result))
+
+result.close()
