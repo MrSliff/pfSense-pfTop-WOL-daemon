a44a17240f97df90b84041d57cb4813da2970521
MrSliff <larsmie@unitybox.de>
2022-09-25 16:27:06 +0200

    first tests

--- daemon.py
+++ daemon.py
@@ -2,6 +2,6 @@ import os
 
 host = "192.168.20.3"
 
-result = os.popen('pftop -b -a -f "dst host "' + host).readlines()
+result = os.popen('pftop -b -a -f "dst host "' + host)
 
-print (result)
+print (result.read())
