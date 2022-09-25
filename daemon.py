9b1bc5457c7c64d779a78030e0b67ada581f32fe
MrSliff <larsmie@unitybox.de>
2022-09-25 18:59:36 +0200

    daemonize script

--- daemon.py
+++ daemon.py
@@ -1,8 +1,25 @@
 import os
+from daemonize import Daemonize
+from time import sleep
 
 host = "192.168.20.3"
 
-#result = os.popen('pftop -b -a -f "dst host "' + host + ' | grep 4:4')
-output = os.popen('pftop -b -a -f "dst host "' + host).read().split()
+sleep_time = 1
 
-print (output[1])
+pid = "/tmp/pftop_wake.pid"
+
+def main ():
+    while True:
+        
+        #output = os.popen('pftop -b -a -f "dst host "' + host + ' | grep 4:4')
+        output = os.popen('pftop -b -a -f "dst host "' + host + ' | grep 2:0').readlines()
+
+        for i in output:
+            print (i)
+
+        print (len(output))
+        
+        sleep(sleep_time)
+
+daemon = Daemonize(app="pftop_wake", pid=pid, action=main)
+daemon.start()
