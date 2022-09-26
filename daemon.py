b6e30ecda9d3399ab7ad849330991ec0f5f87efa
MrSliff <larsmie@unitybox.de>
2022-09-26 20:30:09 +0200

    work on mqtt-cleint

--- daemon.py
+++ daemon.py
@@ -10,7 +10,7 @@ host = "192.168.20.3" # Which host IP to listen for activity
 
 mqtt_config = ["host": "192.168.20.35", "port": 1883, "keepalive": 60] # Set up mqtt
 
-mqtt_device_topic = [""]
+mqtt_device_topic = ["unraid_wakeup"]
 mqtt_subtopic = ["state": "/state", "wakeup": "/wakeup"]
 
 sleep_time = 1 #Set sleep time for daemon in seconds (standard 1s)
@@ -23,6 +23,9 @@ pid = "/tmp/pftop_wake.pid"
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
+
+mqtt_state_topic = mqtt_device_topic + mqtt_subtopic["state"]
+mqtt_wakeup_topic = mqtt_device_topic + mqtt_subtopic["wakeup"]
 
 mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect
